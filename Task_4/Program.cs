fd1afaa746ab81c3923b6eb250441b7bc65d4074
Alex <alexchurilov@mail.ru>
2022-05-02 13:13:12 +0300

    Add Task_4

---
+++ Task_4/Program.cs
@@ -0,0 +1,10 @@
+﻿// Ввод число N
+// Вывод показать промежутки от -N до N
+Console.WriteLine("Введите целое число");
+int N = int.Parse(Console.ReadLine());
+int Y = -N;
+while(Y <= N)
+{
+    Console.WriteLine(Y);
+    Y++;
+}
