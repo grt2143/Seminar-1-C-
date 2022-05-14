ec8ea782b9f0f3ee7c1216a2a44f7512d825167c
Alex <alexchurilov@mail.ru>
2022-05-14 12:57:04 +0300

    Add Task_11 with method

---
+++ Task_11/Program.cs
@@ -0,0 +1,14 @@
+﻿// Из случайного 3х значного числа удалить вторую цифру
+
+int slNumber = new Random().Next(100, 1000);
+Console.WriteLine($"Number = {slNumber}");
+int finalNumber = FindNumber (slNumber);
+Console.WriteLine($"Final Number = {finalNumber}");
+
+int FindNumber (int num)
+{
+    int firstDigit = num / 100;
+    //int secondDigit = (num / 10) % 10;
+    int thirdDigit = num % 10;
+    return ((num / 100) * 10 + num % 10);
+}
