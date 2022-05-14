5d2d0b7ce5c8458d5020d7da7aaa66cc39620ded
Alex <alexchurilov@mail.ru>
2022-05-14 13:13:49 +0300

    Add Task 12 (If, else)

---
+++ Task_12/Program.cs
@@ -0,0 +1,11 @@
+﻿// Принимает на вход 2 числа
+// Выводить является ли первое число кратным второму
+// Если не кратно, то вывести остаток от деления
+
+int number1 = int.Parse(Console.ReadLine());
+int number2 = int.Parse(Console.ReadLine());
+
+if (number1 % number2 == 0)
+    Console.WriteLine("кратно");
+else
+    Console.WriteLine($"не кратно, остаток {number1 % number2}");
