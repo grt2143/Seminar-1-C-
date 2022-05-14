c058fe6f89508cea9e14f40468ad5b57ba8085ea
Alex <alexchurilov@mail.ru>
2022-05-14 14:46:05 +0300

    Add Task 16 (If, else)

---
+++ Task_16/Program.cs
@@ -0,0 +1,15 @@
+﻿// Принимаем на вход 2 числа и проверяем является ли одно число квадратом другого?
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8, 9 -> нет
+
+Console.WriteLine("Введите число 1");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+int number2 = int.Parse(Console.ReadLine());
+
+if (number1 * number1 == number2 || number2 * number2 == number1)
+    Console.WriteLine("да");
+else
+    Console.WriteLine("нет");
