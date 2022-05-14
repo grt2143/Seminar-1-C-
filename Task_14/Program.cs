c49bf738eb94dcd9e2b4387f9fb4432b7181d746
Alex <alexchurilov@mail.ru>
2022-05-14 13:56:40 +0300

    Add Task 14 (If, else)

---
+++ Task_14/Program.cs
@@ -0,0 +1,8 @@
+﻿// Программа принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23
+
+int number = int.Parse(Console.ReadLine());
+
+if (number % 7 == 0 && number % 23 == 0)
+    Console.WriteLine("Yes");
+else
+    Console.WriteLine("No");
